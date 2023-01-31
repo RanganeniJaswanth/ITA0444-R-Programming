def0125fcab4fa34a3e8a1cc447bafe36accb3f5
RanganeniJaswanth <ranganenijaswanth4065.sse@saveetha.com>
2023-01-31 09:31:41 +0530

    Add files via upload

---
+++ 22 question.R
@@ -0,0 +1,8 @@
+my.commuteTime=c(17,16,20,24,22,15,21,15,17,22)
+max(my.commute times)
+mean(my.commuteTimes)
+min(my.commuteTimes)
+my.commuteTimes[14]=18
+mean(my.commuteTimes)
+sum(my.commuteTimes>=20)
+sum(my.commuteTimes<17)/length(my.commuteTimes)*100
