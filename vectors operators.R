85a917c6fc3960192bcbcddfe859e588da54e0e1
RanganeniJaswanth <ranganenijaswanth4065.sse@saveetha.com>
2023-01-31 09:03:05 +0530

    Add files via upload

---
+++ vectors operators.R
@@ -0,0 +1,10 @@
+vec1 <- c(0,2)
+vec2 <- c(2,3)
+
+# performing operations on the operands
+cat ("addition of vectors :",vec1 + vec2,"\n")
+cat ("subtraction of vectors :",vec1 - vec2,"\n")
+cat ("multiplication of vectors :",vec1 * vec2,"\n")
+cat ("division of vectors :",vec1 / vec2,"\n")
+cat ("modulo of vectors :",vec1 ** vec2,"\n")
+cat ("power operator :",vec1 ^ vec2)
