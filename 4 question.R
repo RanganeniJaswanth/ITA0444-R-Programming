538e28ecb52cd48647b946f655cd781258c85ce4
RanganeniJaswanth <ranganenijaswanth4065.sse@saveetha.com>
2023-01-31 10:32:12 +0530

    Create 4 question.R

---
+++ 4 question.R
@@ -0,0 +1,17 @@
+answer1 <- factorial(4)   
+answer3 <- factorial(0)   
+print(answer1) 
+print(answer3) 
+
+
+
+fruits <- c("orange", "mango", "apple", "watermelon", "banana")
+juices <- c("appy", "fruity", "slice")
+milkshakes <- c("mango", "papaya", "chocolate", "rose")
+shop <- data.frames("fruits", "juices", "milkshakes")
+shop
+
+
+
+v <- c(90, 50, 70, 80, 70, 60, 20, 30, 80, 90, 20)
+mean(v)
